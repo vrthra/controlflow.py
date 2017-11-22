22ab610de16987786b66e728af6054613d395eee
Rahul Gopinath <rahul@gopinath.org>
2017-11-22 05:46:04 -0800

    update

--- computedistance.py
+++ computedistance.py
@@ -8,10 +8,14 @@ import example
 import pycfg
 import math
 
-def branch_distance(target, pdict, arcs, seen):
-    arc_first = [i for (i,j) in arcs]
-    arc_second = [j for (i,j) in arcs]
-    # any target is a single line number. go up the child->parent chain until
+def compute_cost(parent, target, arcs, pdict):
+    return 1
+
+def branch_distance(parent, target, pdict, arcs, seen):
+    all_parents = [i for (i,j) in arcs]
+    all_targets = [j for (i,j) in arcs]
+    # any target, parent is a pair of line numbers.
+    # go up the child->parent chain until
     # we get a target branch pair with (condition, next statement).
     # to compute branch distance, we look up if this pair exists in the cdata.arcs.
     # if not, retrieve condition's parents, and for each parent:
@@ -19,13 +23,27 @@ def branch_distance(target, pdict, arcs, seen):
     # resulting in another pair with (condition, stmt) see if it is in cdata.arcs
     # and continue
     seen.add(target)
-    if target not in pdict: return math.inf
-    d, cost = pdict[target]
-    if target in arc_second: return cost  # for now assumed to be 1, but need to compute from predicate
-    parents = [p for p in d['parents'] if p not in seen]
-    if not parents: return math.inf
+    parent_dict = pdict[parent]
 
-    return min(branch_distance(p, pdict, arcs, seen) for p in parents) + cost
+    gparents = [p for p in parent_dict['parents'] if p not in seen]
+
+    # if we were executed, then there is no cost
+    if (parent, target) in arcs: return 0
+
+    # we were not executed. So, where did the flow diverge? Is it here?
+    if target in all_targets:
+        return compute_cost(parent, target, arcs, pdict)
+
+    # if we can not go further up, we dont know how close we came
+    if not gparents: return math.inf
+
+    # so go up the chain.
+    pcost = min(branch_distance(gp, parent, pdict, arcs, seen) for gp in gparents)
+
+    # the cost of missing a conditional is 1 and that of a non-conditional is 0
+    cost = 1 if len(pdict[target]['children']) > 1 else 0
+
+    return pcost + cost
 
 
 if __name__ == '__main__':
@@ -38,11 +56,5 @@ if __name__ == '__main__':
     f = cdata.measured_files()[0]
     arcs = cdata.arcs(f)
     cfg = dict(pycfg.get_cfg('example.py'))
-    pdict = {}
-    for k,v in cfg.items():
-        children = v['children']
-        if len(children) > 1:
-            pdict[k] = (v, 1)
-        else:
-            pdict[k] = (v, 0)
-    print('branch distance:',branch_distance(int(sys.argv[1]), pdict, arcs, set()))
+    parent, target = int(sys.argv[1]),int(sys.argv[2])
+    print('branch distance(parent:%d, target:%d): %d'%(parent, target, branch_distance(parent, target, cfg, arcs, set())))
